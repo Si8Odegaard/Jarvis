e4e389f62d43a0154a164105f512fecad5a29536
Si8Odegaard <powerfootballsj@gmail.com>
2026-06-07 21:55:21 -0500

    Deploy Sun Jun  7 21:55:21 CDT 2026

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE = 'dashboard-pwa-v7';
+const CACHE = 'dashboard-pwa-v8';
 const ASSETS = [
   './', './index.html', './manifest.json', './icon.svg',
-  './topbar.js', './tabs.js', './gym.html', './stack.html', './po-water.html', './nutrition.html', './soccer.html',
+  './topbar.js', './tabs.js', './gym.html', './stack.html', './nutrition.html', './soccer.html',
 ];
 
 self.addEventListener('install', (event) => {
